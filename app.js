5634d61fe1ce17d61bc7ed2c412f4c71bc885422
Roman Mamchyk <Roman_Mamchyk@epam.com>
2017-10-17 18:27:36 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// GLOBAL because it's used everywhere
+path = require('path');
+
+var express = require('express');
+var app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+
+app.get('/', function(req, res){
+    res.send("Hi there! Can I help you with transfering data from MSSQL to Mongo db.\n");
+});
+
+app.listen(8181, function(){
+	console.log('Listening on port 8181');
+});
+
+module.exports = app;
+
+// Set absolute paths for partials
+app.locals.basedir = path.join(__dirname, '');
