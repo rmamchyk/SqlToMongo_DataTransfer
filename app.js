50c7f75dc8df8505ebcb624ed87aa3cd15619c98
Roman Mamchyk <Roman_Mamchyk@epam.com>
2017-10-17 19:11:08 +0300

    Added sql db connection

--- app.js
+++ app.js
@@ -8,11 +8,39 @@ var app = express();
 var mongodbConn = require(path.join(__dirname, 'services', 'mongoUtil'));
 mongodbConn.connect();
 
+// mongo db and sql db connection
+var sql = require("mssql");
+var mongo = require("mongodb");
+
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 
 app.get('/', function(req, res){
     res.send("Hi there! Can I help you with transfering data from MSSQL to Mongo db.\n");
+});
+
+app.get('/categories', function (req, res) {
+    // config for your SQL database
+    var config = {
+        user: 'ChupUser',
+        password: 'chup',
+        server: 'EPUAKYIST0007', 
+        database: 'GadgetLineDb' 
+    };
+    // connect to your SQL database
+    sql.connect(config, function (err) {
+        if (err) console.log(err);
+        var request = new sql.Request();
+        request.query('SELECT * FROM dbo.Categories', function (err, recordset) {
+            if (err) {
+            	console.log(err)
+			} else {
+				
+			}
+            // send records as a response
+            res.send(recordset);
+        });
+    });
 });
 
 app.listen(8181, function(){
