d4212b11fc9b4d932db1aabd29c8d45a85b730d8
Roman Mamchyk <Roman_Mamchyk@epam.com>
2017-10-17 19:20:12 +0300

    Implemented /sql/categories and /sql/products methods

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ path = require('path');
 var express = require('express');
 var app = express();
 
-// Connect to Mongo
+// mongo configuration
 var mongodbConn = require(path.join(__dirname, 'services', 'mongoUtil'));
 mongodbConn.connect();
 
-// mongo db and sql db connection
+// sql configuration
+var sqlConfig = {
+        user: 'ChupUser',
+        password: 'chup',
+        server: 'EPUAKYIST0007', 
+        database: 'GadgetLineDb' 
+    };
+
+
+// sql connection
 var sql = require("mssql");
+
+// mongo connection
 var mongo = require("mongodb");
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
@@ -19,26 +30,28 @@ app.get('/', function(req, res){
     res.send("Hi there! Can I help you with transfering data from MSSQL to Mongo db.\n");
 });
 
-app.get('/categories', function (req, res) {
-    // config for your SQL database
-    var config = {
-        user: 'ChupUser',
-        password: 'chup',
-        server: 'EPUAKYIST0007', 
-        database: 'GadgetLineDb' 
-    };
-    // connect to your SQL database
-    sql.connect(config, function (err) {
+app.get('/sql/categories', function (req, res) {
+    sql.connect(sqlConfig, function (err) {
         if (err) console.log(err);
         var request = new sql.Request();
         request.query('SELECT * FROM dbo.Categories', function (err, recordset) {
-            if (err) {
-            	console.log(err)
-			} else {
-				
-			}
+            if (err) console.log(err);
             // send records as a response
             res.send(recordset);
+            sql.close();
+        });
+    });
+});
+
+app.get('/sql/products', function (req, res) {
+    sql.connect(sqlConfig, function (err) {
+        if (err) console.log(err);
+        var request = new sql.Request();
+        request.query('SELECT * FROM dbo.Products', function (err, recordset) {
+            if (err) console.log(err);
+            // send records as a response
+            res.send(recordset);
+            sql.close();
         });
     });
 });
